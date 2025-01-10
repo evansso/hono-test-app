57a6067fab3d0b073a6b05829763cf5b7446f157
Evansso <marsappollo3@gmail.com>
2025-01-10 22:48:45 +0300

    Update ProductCard fetch request to include cache control settings for improved data retrieval.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,9 @@ function ProductSkeleton() {
 }
 
 async function ProductCard() {
-  const res = await fetch("https://hono-server.evansso.workers.dev/");
+  const res = await fetch("https://hono-server.evansso.workers.dev/", {
+    cache: "no-store",
+  });
   const products = (await res.json()) as Products[];
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
