817c233b444d2c3ce87b85c819ec6f44519328d8
Evansso <marsappollo3@gmail.com>
2025-01-10 22:43:58 +0300

    Update dependencies, enhance Tailwind CSS configuration, and refactor Home component to improve product display. Added new color variables and dark mode support in globals.css, and implemented a loading skeleton for product cards.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,68 @@
-import Image from "next/image";
+import { Suspense } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
   return (
     <div>
       <h1>Products</h1>
-      <Products />
+      <div>
+        <Suspense fallback={<ProductSkeleton />}>
+          <ProductCard />
+        </Suspense>
+      </div>
     </div>
   );
 }
 
-
- interface Products {
+interface Products {
   id: number;
   name: string;
   price: number;
   description: string;
- }
+}
 
- async function Products() {
-  const res = await fetch('https://hono-server.evansso.workers.dev/');
-  const products = await res.json() as Products[];
+function ProductSkeleton() {
   return (
-    <div>
-      <h1>Products</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.name}</h2>
-          <p>{product.description}</p>
-          <p>{product.price}</p>
-        </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {Array.from({ length: 8 }).map((_, index) => (
+        <Card key={index}>
+          <CardHeader>
+            <Skeleton className="h-48 w-full" />
+            <Skeleton className="h-48 w-full" />
+          </CardHeader>
+          <CardContent>
+            <Skeleton className="h-48 w-full" />
+          </CardContent>
+        </Card>
       ))}
     </div>
-  )
- }
+  );
+}
+
+async function ProductCard() {
+  const res = await fetch("https://hono-server.evansso.workers.dev/");
+  const products = (await res.json()) as Products[];
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {products.map((product) => (
+        <Card key={product.id}>
+          <CardHeader>
+            <CardTitle>{product.name}</CardTitle>
+            <CardDescription>{product.description}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>{product.price}</p>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+}
